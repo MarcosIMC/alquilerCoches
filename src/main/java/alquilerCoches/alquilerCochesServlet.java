8cf6a4855d5db26c9a3e85117d1514ea96c4c7ed
markensi <markensi9@outlook.com>
2017-10-29 01:26:44 +0100

    Servlet con el controlador de peticion, vistas de los demas web creadas

--- src/main/java/alquilerCoches/alquilerCochesServlet.java
+++ src/main/java/alquilerCoches/alquilerCochesServlet.java
@@ -1,6 +1,10 @@
 package alquilerCoches;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,14 +22,49 @@ public class alquilerCochesServlet extends HttpServlet {
     public alquilerCochesServlet() {
         super();
         // TODO Auto-generated constructor stub
+        
     }
+    
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+		
+		ServletContext context = config.getServletContext();
+		context.setAttribute("base", config.getInitParameter("base"));
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String base = "/JSP/";
+		String url = base + "index.jsp";
+		
+		String peticion = request.getParameter("peticion");
+		
+		if(peticion != null) {
+			switch (peticion) {
+			case "alquiler":
+				url = base + "alquiler.jsp";
+				break;
+				
+			case "vehiculos":
+				url = base + "vehiculos.jsp";
+				break;
+				
+			case "clientes":
+				
+				url = base + "clientes.jsp";
+				break;
+			}
+		}
+		System.out.println("URL "+url);
+		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(url);
+		requestDispatcher.forward(request, response);
 	}
 
 	/**
@@ -35,13 +74,5 @@ public class alquilerCochesServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
-	@Override
-	public void init() throws ServletException {
-		// TODO Auto-generated method stub
-		super.init();
-	}
-
-	
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+
+	<%
+	
+		response.sendRedirect("alquilerMoya");
+	
+	%>
+
 </body>
 </html>
